b694cb720c0c06a7a1397d40e4295722d9e3313a
marante <bugger@bravis.se>
2018-04-03 15:19:03 +0200

    Simon can't decide how he wants it

--- models/log.go
+++ models/log.go
@@ -2,36 +2,50 @@ package models
 
 import "gopkg.in/mgo.v2/bson"
 
-type ActivityData struct {
+/* type ActivityData struct {
 	PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
 	ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
 	SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
-}
+} */
 
 type PlayerLog struct {
 	ID         bson.ObjectId `json:"_id,omitempty"bson:"_id,omitempty"`
 	UserID     string        `json:"userId" bson:"userId"`
 	Activities struct {
 		InVehicle struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"in_vehicle,omitempty" bson:"in_vehicle,omitempty"`
 		InBicycle struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"on_bicycle,omitempty" bson:"bicycle,omitempty"`
 		OnFoot struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"on_foot,omitempty" bson:"on_foot,omitempty"`
 		Walking struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"walking,omitempty" bson:"walking,omitempty"`
 		Running struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"running,omitempty" bson:"running,omitempty"`
 		Still struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"still,omitempty" bson:"still,omitempty"`
 		Unknown struct {
-			ActivityData `json:"ActivityData,omitempty" bson:"ActivityData,omitempty"`
+			PlayedTracks  []string `json:"playedTracks,omitempty" bson:"playedTracks,omitempty"`
+			ActivityCount int      `json:"activityCount,omitempty" bson:"activityCount,omitempty"`
+			SkipCount     int      `json:"skipCount,omitempty" bson:"skipCount,omitempty"`
 		} `json:"unknown,omitempty" bson:"unknown,omitempty"`
 	} `json:"activities"`
 	TotalSkipCount int    `json:"totalSkipCount,omitempty" bson:"totalSkipCount,omitempty"`
